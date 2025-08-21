b3091849a2e54f7146fc85a2860a45fe53bcf6ee
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-08-21 16:27:13 +0100

    Basic setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const indexRouter = require("./routers/indexRouter")
+const port = process.env.PORT || 3000;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use("/", indexRouter);
+
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
