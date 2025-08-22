65567668768f67508673d39b1f2db6d5c0370033
Oluwatobi <oluwatobioreniyi@gmail.com>
2025-08-22 13:35:15 +0100

    Add initial database connection and category retrieval functionality

---
+++ controllers/categories.controller.js
@@ -0,0 +1,10 @@
+const db = require("../db/queries")
+
+async function getAllCategories(req, res) {
+    const categories = await db.getAllCategories();
+    res.render("index", { categories });
+}
+
+module.exports = {
+    getAllCategories
+}
